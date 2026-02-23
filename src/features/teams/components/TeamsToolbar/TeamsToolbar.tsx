9217b3c58f64e5152483f543daddd665b546183e
haifa <haifa.bensalah@esprit.tn>
2026-02-23 10:50:38 +0100

    feat(teams): add ordering and user search support in teams module

--- src/features/teams/components/TeamsToolbar/TeamsToolbar.tsx
+++ src/features/teams/components/TeamsToolbar/TeamsToolbar.tsx
@@ -10,6 +10,8 @@ import type { TeamFilters } from "../../types/teams.type";
 
 const { Text } = Typography;
 
+//test
+
 interface TeamsToolbarProps {
     search: string;
     onSearchChange: (value: string) => void;
